708ac025969e01993b377c959d2f42c2453bd029
hammad131 <74175920+hammad131@users.noreply.github.com>
2021-09-24 16:03:13 +0500

    add cors handlers

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,14 +1,32 @@
 const express = require('express');
-
 const app = express();
-
 const morgan = require('morgan')
+// const bodyParser = require('body-parser')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+// middleware
 app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
+//cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Header", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+    next();
+    
+})
+
+//Routes for handling request
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
